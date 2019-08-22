8494042b23bb04bfce514ad16c6ca8273cd7eadd
Radhikesh Ranadive <rradhikesh@gmail.com>
2019-08-22 11:54:07 -0500

    Update code.R

--- code.R
+++ code.R
@@ -1,4 +1,4 @@
-### converting factors to numeric in aricv3_regression
+### converting all factor variable to numeric 
 
 asNumeric <- function(x) as.numeric(as.character(x))
 factorsNumeric <- function(d) modifyList(d, lapply(d[, sapply(d, is.factor)],   
