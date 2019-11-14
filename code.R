92ee4e963a90dc3c7a1a650bfd99aa65fd1cf97e
Radhikesh Ranadive <rradhikesh@gmail.com>
2019-11-14 16:14:11 -0600

    Update code.R

--- code.R
+++ code.R
@@ -3,3 +3,7 @@
 asNumeric <- function(x) as.numeric(as.character(x))
 factorsNumeric <- function(d) modifyList(d, lapply(d[, sapply(d, is.factor)],   
                                                    asNumeric))
+
+### which variables contain how many missing values
+NAcol <- which(colSums(is.na(all)) > 0)
+sort(colSums(sapply(all[NAcol], is.na)), decreasing = TRUE)
